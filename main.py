fixed loading frames and applying curls when on the fly computation is enabled
frame_packer has option to take in rotations as quaternions

main.py
@@ -562,18 +562,18 @@ def update_frame():
     i = g_selected_head_avatar_index
     start = get_start_index(i)
     frames_array = g_frames[i]
-    if frames_array:
+    if g_frame[i]:
         frame = min(g_frame[i], frames_array.shape[0]-1)
         frame_array = frames_array[frame]
         g_head_avatars[i].xyz[:g_n_hair_gaussians[i]] = frame_array[:, :3]
         g_head_avatars[i].rot[:g_n_hair_gaussians[i]] = frame_array[:, 3:7]
+        gaussians.rot[start:start+g_n_hair_gaussians[i], :] = frame_array[:, 3:7]
         xscale = frame_array[:, 7]
         scale = np.dstack([xscale, xscale/10, xscale/10])
         g_head_avatars[i].scale[:g_n_hair_gaussians[i]] = scale
-    update_means(i)
+        update_means(i)
 
 def get_hair_rots_amps_freqs(idx):
-    currdir = os.path.dirname(g_file_paths[idx])
     try:
         arrays = np.load(g_curls_file[idx])
         rxyzs = arrays['values']
@@ -625,6 +625,10 @@ def update_means(head_avatar_index):
             gaussians.rot[start:start+g_n_hair_gaussians[i], :] = rot_curls
             gaussians.scale[start:start+g_n_hair_gaussians[i], :] = scale_curls*g_hair_scale[i]
         else:
+            n_strands = g_n_strands[i]
+            n_gaussians_per_strand = g_n_gaussians_per_strand[i]
+            n_hair_gaussians = g_n_hair_gaussians[i]
+            g_hair_points[i], g_hair_normals[i] = get_hair_points(g_head_avatars[i].xyz, g_head_avatars[i].rot, g_head_avatars[i].scale, n_strands, n_gaussians_per_strand, n_hair_gaussians)
             points = np.copy(g_hair_points[i])
             points[:,:,0] += d
             global_nudging = get_curls(g_wave_amplitude[i], g_wave_frequency[i], g_hair_normals[i], g_n_gaussians_per_strand[i], g_n_strands[i])
@@ -2020,7 +2024,8 @@ def main():
 
                 imgui.text(f"Selected Frames File: {g_frame_file[g_selected_head_avatar_index]}")
 
-                changed, g_frame[i] = imgui.slider_int("Frame", g_frame[i], 0, 100, "Frame = %d")
+                last_frame = g_frames[i].shape[0] if isinstance(g_frames[i], np.ndarray) else 0
+                changed, g_frame[i] = imgui.slider_int("Frame", g_frame[i], 0, last_frame, "Frame = %d")
                 if changed:
                     update_frame()
                     update_avatar_planes()

utils/frame_packer.py
@@ -16,15 +16,27 @@ def main(args):
     n_gaussians = xyz.shape[0]
     
     frame_array = np.zeros((n_frames, n_gaussians*31, 3 + 4 + 1))
-    for frame in range(n_frames):
-        xyz = np.load(f"{args.path}//frame_{str(frame+1)}_mean_frenet.npy").reshape(-1, 3)
-        rot = np.load(f"{args.path}//frame_{str(frame+1)}_rot_frenet.npy").transpose((0, 1, 3, 2))
-        rot = TNB2qvecs(rot[:,:,0], rot[:,:,1], rot[:,:,2]).reshape(-1,4)
-        scale = np.load(f"{args.path}//frame_{str(frame+1)}_scale_frenet.npy").reshape(-1, 3)
 
-        frame_array[frame, :, :3] = xyz
-        frame_array[frame, :, 3:7] = rot
-        frame_array[frame, :, 7] = scale[:,0]
+    if args.rot_format == 'mat':
+        for frame in range(n_frames):
+            xyz = np.load(f"{args.path}//frame_{str(frame+1)}_mean_frenet.npy").reshape(-1, 3)
+            rot = np.load(f"{args.path}//frame_{str(frame+1)}_rot_frenet.npy").transpose((0, 1, 3, 2))
+            rot = TNB2qvecs(rot[:,:,0], rot[:,:,1], rot[:,:,2]).reshape(-1,4)
+            scale = np.load(f"{args.path}//frame_{str(frame+1)}_scale_frenet.npy").reshape(-1, 3)
+
+            frame_array[frame, :, :3] = xyz
+            frame_array[frame, :, 3:7] = rot
+            frame_array[frame, :, 7] = scale
+    else:
+        for frame in range(n_frames):
+            xyz = np.load(f"{args.path}//frame_{str(frame+1)}_mean_frenet.npy").reshape(-1, 3)
+            rot = np.load(f"{args.path}//frame_{str(frame+1)}_rot_frenet.npy").reshape(-1,4)
+            scale = np.load(f"{args.path}//frame_{str(frame+1)}_scale_frenet.npy").flatten()
+
+            frame_array[frame, :, :3] = xyz
+            frame_array[frame, :, 3:7] = rot
+            frame_array[frame, :, 7] = scale
+        
 
     path = os.path.join(os.path.dirname(os.path.dirname(args.path)), "frames.npy")
     np.save(path, frame_array)
@@ -34,6 +46,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(conflict_handler='resolve')
     parser.add_argument('path', type=str)
+    parser.add_argument('--rot_format', choices=['quat', 'mat'], help='Quaternion if your rotation files are as such or mat otherwise', default='mat', type=str)
 
     args, _ = parser.parse_known_args()
     args = parser.parse_args()